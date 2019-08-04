5adf7b7fa7e558100525981d1893e955454996e3
Mike Krüger <mikkrg@microsoft.com>
2019-08-04 17:49:51 +0200

    Added "C" command to console controller.
    It's now easier to set pulses on a single channel.

--- RobotConsole/ConsoleController.cs
+++ RobotConsole/ConsoleController.cs
@@ -5,6 +5,8 @@ namespace RobotControlFramework.MaintainanceConsole
 {
     class ConsoleController : AbstractController
     {
+        int currentChannel = 0;
+
         internal void Run()
         {
             Console.WriteLine("Awaiting commands.");
@@ -19,16 +21,40 @@ namespace RobotControlFramework.MaintainanceConsole
                     case "Q":
                     case "QUIT":
                         return;
+                    case "C":
+                        try
+                        {
+                            currentChannel = int.Parse(split[1]);
+                        } catch (Exception e)
+                        {
+                            Log.Error(e.ToString());
+                            Console.WriteLine("Usage: C [Channel].");
+                        }
+                        break;
                     case "S":
                         try
                         {
                             int channel = int.Parse(split[1]);
                             int pulse = int.Parse(split[2]);
                             Command.SingleServo(channel, pulse).Execute();
-                        } catch (Exception e)
+                        }
+                        catch (Exception e)
                         {
                             Log.Error(e.ToString());
                             Console.WriteLine("Usage: S [Channel] [Pulse]");
+                        }
+                        break;
+
+                    default:
+                        try
+                        {
+                            int pulse = int.Parse(split[0]);
+                            Command.SingleServo(currentChannel, pulse).Execute();
+                        }
+                        catch (Exception e)
+                        {
+                            Log.Error(e.ToString());
+                            Console.WriteLine("Usage: [Pulse]");
                         }
                         break;
                 }

8e9cd585155b6dabbbf2e4888a84d86195a2ad16
Mike Krüger <mikkrg@microsoft.com>
2020-10-06 12:42:38 +0200

    elvcelvelvelvelvelv

--- HexBot/Properties/AssemblyInfo.cs
+++ HexBot/Properties/AssemblyInfo.cs
@@ -2,9 +2,7 @@
 using System.Runtime.CompilerServices;
 
 // Information about this assembly is defined by the following attributes. 
-// Change
-
-// them to the values specific to your project.
+// Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("HexBot")]
 [assembly: AssemblyDescription("")]

97836ee4ac6368907ef0392c2fea1a51fba761d9
Mike Krüger <mikkrg@microsoft.com>
2022-11-04 08:55:01 +0100

    evevlevlevl

---
+++ TestConsoleProject/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+int i = 42;
+float f = 32;
+
+while (true)
+{
+    Console.WriteLine($"int: {i} float:{f}");
+    var line = Console.ReadLine();
+    if (line.StartsWith("f"))
+    {
+        line = line.Substring(1);
+        f = float.Parse(line);
+    } else
+    {
+        if (int.TryParse(line, out var result))
+        {
+            i = result;
+        }
+    }
+}
+
